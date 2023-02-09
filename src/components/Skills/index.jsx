bd4101ce86090b823ae2266337a62dd8b6e5d92c
danirayGOD <mrdaniraymay@gmail.com>
2023-02-08 23:10:05 -0500

    termine :D

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import Card from "./Card";
+// import Card from "./Card";
 const Container = styled.div`
-  /* background-color: purple; */
   width: 100%;
   height: 100vh;
   padding: 0 300px;
@@ -23,66 +22,13 @@ const Container = styled.div`
   @media (max-width: 400px) {
     padding: 0 10px;
   }
-  /* width: 100%;
-  height: 100vh;
-  z-index: 3;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  h2 {
-    text-align: center;
-    font-size: 5rem;
-    color: ${(props) => props.theme.primaryColor};
-    text-shadow: 0px 0px 30px ${(props) => props.theme.primaryColorGlow2};
-  }
-  @media (max-width: 800px) {
-    height: max-content;
-    h2 {
-      font-size: 3rem;
-    }
-  }
-  @media (max-width: 400px) {
-    padding-top: 80px;
-    h2 {
-      font-size: 3rem;
-    }
-  } */
 `;
-// const CardsContainer = styled.div`
-//   margin-top: 40px;
-//   width: 100%;
-//   display: grid;
-//   grid-template-columns: repeat(5, 180px);
-//   grid-auto-rows: 130px;
-//   justify-content: center;
-//   gap: 30px;
-//   position: relative;
-//   z-index: 3;
-//   @media (max-width: 1200px) {
-//     grid-template-columns: repeat(4, 150px);
-//     grid-auto-rows: 100px;
-//   }
-//   @media (max-width: 800px) {
-//     grid-template-columns: repeat(4, 80px);
-//     grid-auto-rows: 80px;
-//     gap: 15px;
-//     padding: 0 20px;
-//   }
-//   @media (max-width: 400px) {
-//     grid-template-columns: repeat(3, 70px);
-//     grid-auto-rows: 70px;
-//     padding: 0px;
-//     margin-top: 20px;
-//   }
-// `;
 const ContainerInformation = styled.div`
   width: 50%;
-  height: 100%;
-  /* background-color: blue; */
+  height: max-content;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  /* align-items: ; */
   h3 {
     font-size: clamp(1rem, 3vw, 2rem);
     transition: color 0.4s;
@@ -177,7 +123,8 @@ const ContainerGif = styled.div`
 `;
 
 function Skills({ id }) {
-  const myH2 = useRef();
+  const myContainer = useRef();
+  const myGif = useRef();
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
@@ -188,49 +135,17 @@ function Skills({ id }) {
         }
       });
     });
-    observer.observe(myH2.current);
+    observer.observe(myContainer.current);
+    observer.observe(myGif.current);
   }, []);
   return (
     <Container id={id}>
-      {/* <h2
-        ref={myH2}
-        style={{
-          opacity: isVisible ? 1 : 0,
-          transform: isVisible ? "translateY(0)" : "translateY(100%)",
-          transition: "opacity 0.8s, transform 0.8s",
-        }}
-      >
-        Skills
-      </h2>
-      <CardsContainer
-        style={{
-          opacity: isVisible ? 1 : 0,
-          transform: isVisible ? "translateY(0)" : "translateY(100%)",
-          transition: "opacity 0.9s, transform 0.9s",
-        }}
-      >
-        <Card url="/tech/html.png" title="html"></Card>
-        <Card url="/tech/css.png" title="css"></Card>
-        <Card url="/tech/javascript.png" title="javascript"></Card>
-        <Card url="/tech/jquery.png" title="jquery"></Card>
-        <Card url="/tech/unkwow.png" title="bootstrap"></Card>
-        <Card url="/tech/sass.png" title="sass"></Card>
-        <Card url="/tech/git.png" title="git"></Card>
-        <Card url="/tech/react.png" title="react"></Card>
-        <Card url="/tech/styledComponents.png" title="styled Components"></Card>
-        <Card url="/tech/typescript.png" title="typeScript"></Card>
-        <Card url="/tech/material.png" title="material ui"></Card>
-        <Card url="/tech/next.png" title="next"></Card>
-        <Card url="/tech/nodejs.webp" title="nodejs"></Card>
-        <Card url="/tech/figma.png" title="figma"></Card>
-        <Card url="/tech/photoshop.png" title="photoshop"></Card>
-      </CardsContainer> */}
       <ContainerInformation
-        ref={myH2}
+        ref={myContainer}
         style={{
           opacity: isVisible ? 1 : 0,
           transform: isVisible ? "translateY(0)" : "translateY(100%)",
-          transition: "opacity 0.8s, transform 0.8s",
+          transition: "opacity 0.8s, transform 0.7s",
         }}
       >
         <h3>▹ Habilidades</h3>
@@ -281,6 +196,7 @@ function Skills({ id }) {
       </ContainerInformation>
 
       <ContainerGif
+      ref={myGif}
         style={{
           opacity: isVisible ? 1 : 0,
           transform: isVisible ? "translateY(0)" : "translateY(100%)",
